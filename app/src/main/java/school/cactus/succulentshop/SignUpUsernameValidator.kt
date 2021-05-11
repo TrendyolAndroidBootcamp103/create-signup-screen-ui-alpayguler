e61f764b69ef545724f01e18de9b0e1c0a583958
alpayguler <alpayguler19@gmail.com>
2021-05-11 15:28:23 +0300

    Fix short username based on feedback.
    Fix short username bug based on feedback.

--- app/src/main/java/school/cactus/succulentshop/SignUpUsernameValidator.kt
+++ app/src/main/java/school/cactus/succulentshop/SignUpUsernameValidator.kt
@@ -8,7 +8,7 @@ class SignUpUsernameValidator : Validator {
     override fun validate(field: String) =
         when {
             field.isEmpty() -> R.string.username_is_required
-            field.length < 2 -> R.string.username_is_too_short
+            field.length < 3 -> R.string.username_is_too_short
             field.length > 20 -> R.string.username_is_too_long
             usernamePattern.toRegex().matches(field).not() -> R.string.username_must_not_consist_characters_except_az_09_
             else -> null
